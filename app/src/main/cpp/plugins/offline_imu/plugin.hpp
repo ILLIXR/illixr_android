5272be049603398b22fd5a7c52f17da4f51c1ebf
astro-friedel <friedel@illinois.edu>
2025-06-26 10:12:07 -0500

    updated offline_imu structure

---
+++ app/src/main/cpp/plugins/offline_imu/plugin.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "illixr/data_format/imu.hpp"
+#include "illixr/threadloop.hpp"
+#include "illixr/switchboard.hpp"
+#include "illixr/phonebook.hpp"
+
+#include "data_loading.hpp"
+
+namespace ILLIXR {
+class offline_imu : public ILLIXR::threadloop {
+public:
+    [[maybe_unused]] offline_imu(const std::string& name_, phonebook* pb_);
+
+protected:
+    skip_option _p_should_skip() override;
+
+    void _p_one_iteration() override;
+
+private:
+    const std::map<ullong, sensor_types> sensor_data_;
+    std::map<ullong, sensor_types>::const_iterator sensor_data_it_;
+    const std::shared_ptr<switchboard> switchboard_;
+    switchboard::writer<data_format::imu_type> imu_;
+
+    // Timestamp of the first IMU value from the dataset
+    ullong dataset_first_time_;
+    // Current IMU timestamp
+    ullong dataset_now_;
+
+    record_coalescer imu_cam_log_;
+
+    std::shared_ptr<RelativeClock> clock_;
+};
+
+}
