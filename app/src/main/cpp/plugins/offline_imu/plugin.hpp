9993b89ebdb1e3b42abd14c9c6eec27401571efb
astro-friedel <friedel@illinois.edu>
2025-06-30 16:40:47 -0500

    sync'd offline_imu with mainline

--- app/src/main/cpp/plugins/offline_imu/plugin.hpp
+++ app/src/main/cpp/plugins/offline_imu/plugin.hpp
@@ -1,28 +1,39 @@
 #pragma once
 
 #include "illixr/data_format/imu.hpp"
-#include "illixr/threadloop.hpp"
-#include "illixr/switchboard.hpp"
+#include "illixr/data_format/misc.hpp"
+#include "illixr/managed_thread.hpp"
 #include "illixr/phonebook.hpp"
+#include "illixr/relative_clock.hpp"
+#include "illixr/switchboard.hpp"
+#include "illixr/threadloop.hpp"
 
-#include "data_loading.hpp"
+#include <map>
 
 namespace ILLIXR {
-class offline_imu : public ILLIXR::threadloop {
+
+typedef struct {
+    Eigen::Vector3d angular_v;
+    Eigen::Vector3d linear_a;
+} raw_imu_type;
+
+typedef struct {
+    raw_imu_type imu0;
+} sensor_types;
+
+class offline_imu : public threadloop {
 public:
-    [[maybe_unused]] offline_imu(const std::string& name_, phonebook* pb_);
+    [[maybe_unused]] offline_imu(const std::string& name, phonebook* pb);
 
 protected:
     skip_option _p_should_skip() override;
-
-    void _p_one_iteration() override;
+    void        _p_one_iteration() override;
 
 private:
-    const std::map<ullong, sensor_types> sensor_data_;
+    const std::shared_ptr<switchboard>             switchboard_;
+    const std::map<ullong, sensor_types>           sensor_data_;
     std::map<ullong, sensor_types>::const_iterator sensor_data_it_;
-    const std::shared_ptr<switchboard> switchboard_;
-    std::shared_ptr<const relative_clock> clock_;
-    switchboard::writer<data_format::imu_type> imu_;
+    switchboard::writer<data_format::imu_type>     imu_;
 
     // Timestamp of the first IMU value from the dataset
     ullong dataset_first_time_;
@@ -30,6 +41,7 @@ private:
     ullong dataset_now_;
 
     record_coalescer imu_cam_log_;
-};
 
-}
+    std::shared_ptr<relative_clock> clock_;
+};
+} // namespace ILLIXR
