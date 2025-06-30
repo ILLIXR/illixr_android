ec1e803e671ec6ec5682b64c43cde10fe0343b0a
astro-friedel <friedel@illinois.edu>
2025-06-30 16:42:41 -0500

    sync'd pose_lookup with mainline

--- app/src/main/cpp/services/pose_lookup/service.hpp
+++ app/src/main/cpp/services/pose_lookup/service.hpp
@@ -1,47 +1,33 @@
 #pragma once
 
-#include "illixr/data_format/misc.hpp"
 #include "illixr/data_format/pose_prediction.hpp"
-
-#include "data_loading.hpp"
-#include "utils.hpp"
+#include "illixr/data_loading.hpp"
+#include "illixr/global_module_defs.hpp"
+#include "illixr/phonebook.hpp"
+#include "illixr/plugin.hpp"
 
 namespace ILLIXR {
+
+typedef data_format::pose_type sensor_types;
+
 class pose_lookup_impl : public data_format::pose_prediction {
 public:
-    pose_lookup_impl(const phonebook* const pb);
-
+    explicit pose_lookup_impl(const phonebook* const pb);
     data_format::fast_pose_type get_fast_pose() const override;
-
-    data_format::pose_type get_true_pose() const override {
-        throw std::logic_error{"Not Implemented"};
-    }
-
-    bool fast_pose_reliable() const override {
-        return true;
-    }
-
-    bool true_pose_reliable() const override {
-        return false;
-    }
-
-    Eigen::Quaternionf get_offset() override {
-        return offset_;
-    }
-
-    data_format::pose_type correct_pose(const data_format::pose_type pose) const override;
-
-    void set_offset(const Eigen::Quaternionf& raw_o_times_offset) override;
-
-    Eigen::Quaternionf apply_offset(const Eigen::Quaternionf& orientation) const;
-
+    data_format::pose_type      get_true_pose() const override;
+    bool                        fast_pose_reliable() const override;
+    bool                        true_pose_reliable() const override;
+    Eigen::Quaternionf          get_offset() override;
+    data_format::pose_type      correct_pose(const data_format::pose_type& pose) const override;
+    void                        set_offset(const Eigen::Quaternionf& raw_o_times_offset) override;
+    Eigen::Quaternionf          apply_offset(const Eigen::Quaternionf& orientation) const;
     data_format::fast_pose_type get_fast_pose(time_point time) const override;
 
 private:
-    const std::shared_ptr<switchboard>         switchboard_;
+    const std::shared_ptr<switchboard>          switchboard_;
     const std::shared_ptr<const relative_clock> clock_;
-    mutable Eigen::Quaternionf                 offset_{Eigen::Quaternionf::Identity()};
-    mutable std::shared_mutex                  offset_mutex_;
+    mutable Eigen::Quaternionf                  offset_{Eigen::Quaternionf::Identity()};
+    mutable std::shared_mutex                   offset_mutex_;
 
     const std::map<ullong, sensor_types>                        sensor_data_;
     std::map<ullong, sensor_types>::const_iterator              sensor_data_it_;
@@ -56,4 +42,4 @@ private:
     double          align_scale_;
 };
 
-}
+} // namespace ILLIXR
