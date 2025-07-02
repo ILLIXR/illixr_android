9bd8caaa39ab2ba426372006396bac02e62fea72
astro-friedel <friedel@illinois.edu>
2025-07-02 16:03:26 -0500

    fixed hmd code to match mainline

--- app/src/main/cpp/utils/hmd.hpp
+++ app/src/main/cpp/utils/hmd.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <GLES/gl.h>
 
+namespace ILLIXR {
 // HMD utility class for warp mesh structs, spline math, etc
 class HMD {
 public:
@@ -16,40 +17,40 @@ public:
         GLfloat y;
     };
 
-    struct mesh_coord3d_t {
+    struct [[maybe_unused]] mesh_coord3d_t {
         GLfloat x;
         GLfloat y;
         GLfloat z;
     };
 
-    struct uv_coord_t {
+    struct [[maybe_unused]] uv_coord_t {
         GLfloat u;
         GLfloat v;
     };
 
     struct hmd_info_t {
-        int displayPixelsWide;
-        int displayPixelsHigh;
-        int tilePixelsWide;
-        int tilePixelsHigh;
-        int eyeTilesWide;
-        int eyeTilesHigh;
-        int visiblePixelsWide;
-        int visiblePixelsHigh;
-        float visibleMetersWide;
-        float visibleMetersHigh;
-        float lensSeparationInMeters;
-        float metersPerTanAngleAtCenter;
-        int numKnots;
+        [[maybe_unused]] int display_pixels_wide;
+        int display_pixels_high;
+        int tile_pixels_wide;
+        int tile_pixels_high;
+        int eye_tiles_wide;
+        int eye_tiles_high;
+        int visible_pixels_wide;
+        int visible_pixels_high;
+        float visible_meters_wide;
+        float visible_meters_high;
+        float lens_separation_in_meters;
+        float meters_per_tan_angle_at_center;
+        int num_knots;
         float K[11];
-        float chromaticAberration[4];
+        float chromatic_aberration[4];
     };
 
     static float max_float(float x, float y);
 
     static float min_float(float x, float y);
 
-    [[maybe_unused]] static float evaluate_catmull_rom_spline(float value, const float* K, int num_knots);
+    static float evaluate_catmull_rom_spline(float value, const float* K, int num_knots);
 
     [[maybe_unused]] static void
     get_default_hmd_info(const int display_pixels_wide, const int display_pixels_high,
@@ -59,6 +60,10 @@ public:
 
     [[maybe_unused]] static void
     build_distortion_meshes(
-            std::array<std::array<std::vector<mesh_coord2d_t>, NUM_COLOR_CHANNELS>, NUM_EYES>& distort_coords,
-            hmd_info_t& hmd_info);
+            std::array <std::array<std::vector < mesh_coord2d_t>, NUM_COLOR_CHANNELS>, NUM_EYES
+
+    >& distort_coords,
+    hmd_info_t& hmd_info
+    );
 };
+}
