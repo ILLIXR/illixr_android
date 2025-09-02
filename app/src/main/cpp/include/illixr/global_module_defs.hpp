e0629baecfc7ce2b897ed7ba4eb6db64e2f45471
astro-friedel <friedel@illinois.edu>
2025-09-02 15:06:51 -0500

    updated resolution for android

--- app/src/main/cpp/include/illixr/global_module_defs.hpp
+++ app/src/main/cpp/include/illixr/global_module_defs.hpp
@@ -13,7 +13,7 @@ namespace ILLIXR {
 /// Display parameters
 struct display_params {
     // Display width in pixels
-    static constexpr unsigned width_pixels = 1080*2;
+    static constexpr unsigned width_pixels = 1120*2;
 
     // Display height in pixels
     static constexpr unsigned height_pixels = 2400;
