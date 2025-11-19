4d80801ce8d2ef0deca27a3012f977c51fa5af7a
Douglas N Friedel <friedel@illinois.edu>
2025-11-19 12:35:31 -0600

    added function for unity to call to start illixr

--- app/src/main/cpp/src/illixr.hpp
+++ app/src/main/cpp/src/illixr.hpp
@@ -15,6 +15,14 @@ constexpr std::chrono::seconds          ILLIXR_RUN_DURATION_DEFAULT{60};
 
 extern ILLIXR::runtime* runtime_;
 
+
+#ifdef UNITY_LIBRARY
+
+extern "C" {
+    void initialize_for_unity(const char* path, TextCallback callback);
+}
+#endif
+
 namespace ILLIXR {
     void run(const std::vector<std::string>& plugins, TextCallback callback);
 
