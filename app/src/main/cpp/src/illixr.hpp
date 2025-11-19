4e8825b7d4b1a9a4b8a8e3c9d5cf9a90eeb5e730
Douglas N Friedel <friedel@illinois.edu>
2025-11-19 12:35:53 -0600

    no need for sleep function in this instance

--- app/src/main/cpp/src/illixr.hpp
+++ app/src/main/cpp/src/illixr.hpp
@@ -25,23 +25,4 @@ extern "C" {
 
 namespace ILLIXR {
     void run(const std::vector<std::string>& plugins, TextCallback callback);
-
-class cancellable_sleep {
-public:
-    template<typename T, typename R>
-    bool sleep(std::chrono::duration<T, R> duration) {
-        auto wake_up_time = std::chrono::system_clock::now() + duration;
-        while (!terminate_.load() && std::chrono::system_clock::now() < wake_up_time) {
-            std::this_thread::sleep_for(std::chrono::milliseconds{100});
-        }
-        return terminate_.load();
-    }
-
-    void cancel() {
-        terminate_.store(true);
-    }
-
-private:
-    std::atomic<bool> terminate_{false};
-};
 } // namespace ILLIXR
